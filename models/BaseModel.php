d213fbcb7f156f7e9c065f70cc38d7f977444e04
j-beere <beere.justin@gmail.com>
2015-08-12 11:23:10 -0500

    models be wack

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -5,8 +5,7 @@ class Model {
     protected static $table;
     protected static $id;
     private $attributes = array();
-    protected $columnNames = array();
-    protected static $dbTables = array();
+
 
     /*
      * Constructor
@@ -14,7 +13,6 @@ class Model {
     public function __construct()
     {
          self::dbConnect();
-        //  self::setTables();
     }
 
     /*
@@ -31,38 +29,12 @@ class Model {
         }
     }
 
-    private static function setTables()
-    {
-        $tableStmt = self::$dbc->query('SHOW TABLES');
-        $tablesArray = $tableStmt->fetchAll();
-        foreach ($tablesArray as $value) {
-            self::$dbTables[]= $value[0];
-        }
-
-    }
 
     public function getTables()
     {
         return self::$dbTables;
     }
 
-    public function getColumnNames()
-    {
-        $columnStmt = self::$dbc->query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
-        WHERE TABLE_NAME = '".static::$table."'");
-        $columnsArray = $columnStmt->fetchAll();
-        foreach ($columnsArray as $value) {
-            $this->columnNames[]= $value['COLUMN_NAME'];
-        }
-        return $this->columnNames;
-    }
-    // private function setDataType()
-    // {
-    //     "SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
-    //     WHERE TABLE_NAME = '$table'
-    //     AND "
-    //
-    // }
     /*
      * Get a value from attributes based on name
      */
@@ -86,104 +58,7 @@ class Model {
     /*
      * Persist the object to the database
      */
-     public function save()
-     {
-         $columns = self::$dbc->query("SELECT `COLUMN_NAME`, `DATA_TYPE`, `COLUMN_KEY`
-                                        FROM `INFORMATION_SCHEMA`.`COLUMNS`
-                                        WHERE `TABLE_SCHEMA` = '" . DB_NAME . "' AND `TABLE_NAME` = '" . static::$table . "';")->fetchAll(PDO::FETCH_ASSOC);
-         if(!empty($this->attributes))
-         {
-             isset($this->attributes[static::$id]) ? $this->update($columns) : $this->insert($columns);
-         }
-     }
 
-     protected function update($columns)
-     {
-         $numItems = count($columns);
-         $i 		  = 0;
-         $append   = '';
-         $query 	  = "UPDATE ".static::$table." SET";
-
-         foreach($columns as $column)
-         {
-             if($column['COLUMN_KEY'] != 'PRI')
-             {
-                 /**
-                 *Assumes the existence via - 1 of one int type element of $columns...
-                 */
-                 (++$i == $numItems - 1) ? $query .= " " .$column['COLUMN_NAME']." = :".$column['COLUMN_NAME'] : $query.= " ". $column['COLUMN_NAME']. " = :".$column['COLUMN_NAME'].",";
-             }
-             else
-             {
-                 $append = " WHERE ".$column['COLUMN_NAME']. " = :".$column['COLUMN_NAME'];
-             }
-
-         }
-
-         $query .= $append;
-
-         $stmt = self::$dbc->prepare($query);
-
-         foreach($this->attributes as $attribute => $value)
-         {
-             $type = (is_numeric($value)) ? PDO::PARAM_INT : PDO::PARAM_STR;
-             $stmt->bindValue(":{$attribute}", $value, $type);
-         }
-
-         $stmt->execute();
-
-
-     }
-
-     protected function insert($columns)
-     {
-         $query	  = 'INSERT INTO ' . static::$table . ' (';
-         $values   = 'VALUES (';
-         $i		  = 0;
-         $numItems = count($columns);
-         //var_dump($numItems);
-
-         foreach($columns as $column)
-         {
-             if($column['COLUMN_KEY'] != 'PRI')
-             {
-                 $query .= (++$i == $numItems - 1) ? $column['COLUMN_NAME'] . ') ' : $column['COLUMN_NAME'] . ', ';
-
-                 if($column['DATA_TYPE'] != 'date')
-                 {
-
-                     $values .= ($i == $numItems - 1) ? ':' . $column['COLUMN_NAME'] . ');' : ':' . $column['COLUMN_NAME'] . ', ';
-                 }
-                 else
-                 {
-                     $values .= ($i == $numItems - 1) ? 'cast(:' . $column['COLUMN_NAME'] . ' as DATE));' : ' cast(:' . $column['COLUMN_NAME'] . ' as DATE), ';
-                 }
-             }
-
-         }
-
-         $query .= $values;
-
-         $stmt = self::$dbc->prepare($query);
-         print($query);
-         print_r($this->attributes);
-
-         foreach($this->attributes as $attribute => $value)
-         {
-             $type = (is_numeric($value)) ? PDO::PARAM_INT : PDO::PARAM_STR;
-             $stmt->bindValue(":{$attribute}", $value, $type);
-         }
-
-         $stmt->execute();
-
-         $stmt = self::$dbc->prepare("SELECT " . static::$id . " FROM " . static::$table . " ORDER BY " . static::$id . " DESC LIMIT 1;");
-
-         $stmt->execute();
-
-         $data = $stmt->fetch(PDO::FETCH_ASSOC);
-         $this->attributes[static::$id] = $data[static::$id];
-
-     }
     /*
      * Find a record based on an id
      */
